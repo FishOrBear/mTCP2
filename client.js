257b34beb518cf58dfdf0e5d5d70acde05b902ef
Mike Erickson <cay776@gmail.com>
2024-10-21 10:40:14 +0800

    默认为2个连接聚合,保证了提速效果,避免太多连接竞争

--- client.js
+++ client.js
@@ -6,5 +6,5 @@ let upstream_host = "0.0.0.0"; //远端ip()
 let upstream_port = 15201; //远端mtcp端口
 let lisent_port = 5201; //本地监听端口
 //配置tcp连接个数(每个tcp连接单独一个池子)
-mtcp_1.MSocket.PoolCount = 4; //默认为4 (根据带宽选择 不建议超过5)
+mtcp_1.MSocket.PoolCount = 2; //默认为2 (根据带宽选择 不建议超过5)
 (0, pipeServer_1.createPipeServer)(upstream_port, upstream_host, pipeServer_1.PipeType.tcp2mtcp, lisent_port);
