f46643402ff812a47d30871a9db0ca02ec8af9c0
ChenX <maple6@vip.qq.com>
2024-10-22 16:53:57 +0800

    使用预连接池,避免握手带来的损耗(0RTT)

--- client.js
+++ client.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mtcp_1 = require("./mtcp");
 const pipeServer_1 = require("./pipeServer");
-let upstream_host = "0.0.0.0"; //远端ip()
+let upstream_host = "8.8.8.8"; //远端ip()
 let upstream_port = 15201; //远端mtcp端口
 let lisent_port = 5201; //本地监听端口
 //配置tcp连接个数(每个tcp连接单独一个池子)
 mtcp_1.MSocket.PoolCount = 2; //默认为2 (根据带宽选择 不建议超过5)
-(0, pipeServer_1.createPipeServer)(upstream_port, upstream_host, pipeServer_1.PipeType.tcp2mtcp, lisent_port);
+let preLinkCount = 10; //预连接池
+(0, pipeServer_1.createPipeServer)(upstream_port, upstream_host, pipeServer_1.PipeType.tcp2mtcp, lisent_port, mtcp_1.MSocket.PoolCount, preLinkCount);
--- pipeServer.js
+++ pipeServer.js
@@ -5,21 +5,71 @@ exports.createPipeServer = createPipeServer;
 const net_1 = require("net");
 const mtcp_1 = require("./mtcp");
 var PipeType;
-(function (PipeType) {
+(function (PipeType)
+{
     PipeType[PipeType["tcp2mtcp"] = 0] = "tcp2mtcp";
     PipeType[PipeType["mtcp2tcp"] = 1] = "mtcp2tcp";
 })(PipeType || (exports.PipeType = PipeType = {}));
-function createPipeServer(upstream_port, upstream_host, pipe_type = PipeType.tcp2mtcp, lisent_port, poolCount = 4) {
+function createPipeServer(upstream_port, upstream_host, pipe_type = PipeType.tcp2mtcp, lisent_port, poolCount = 2, //每个连接的子流个数
+    preLinkCount = 0)
+{
     let createServerFn = pipe_type == PipeType.tcp2mtcp ? net_1.createServer : mtcp_1.createMTcpServer;
     let connectFn = pipe_type == PipeType.tcp2mtcp ? mtcp_1.connectMTcp : net_1.connect;
-    let server = createServerFn(function (conn) {
+    let preConnPool = []; //预连接池
+    //预连接
+    const PreConnect = () =>
+    {
+        let conn = connectFn({ port: upstream_port, host: upstream_host }, function ()
+        {
+            preConnPool.push(conn);
+            const remove = () =>
+            {
+                let index = preConnPool.indexOf(conn);
+                if (index !== -1) {
+                    preConnPool.splice(index, 1);
+                    PreConnect();
+                }
+            };
+            conn.on("close", remove);
+            conn.on('error', () =>
+            {
+                remove();
+                conn.destroy();
+            });
+        });
+    };
+    if (pipe_type === PipeType.mtcp2tcp && preLinkCount > 0) {
+        for (let i = 0; i < preLinkCount; i++)
+            setTimeout(PreConnect, i * 500);
+        //每3s新建连接
+        setInterval(() =>
+        {
+            if (preConnPool.length === preLinkCount) {
+                preConnPool.shift();
+                PreConnect();
+            }
+        }, 3000);
+    }
+    let server = createServerFn(function (conn)
+    {
         if (pipe_type === PipeType.tcp2mtcp)
             mtcp_1.MSocket.PoolCount = poolCount;
-        let up_socket = connectFn({ port: upstream_port, host: upstream_host }, function () {
+        let up_socket;
+        if (preConnPool.length) {
+            up_socket = preConnPool.shift();
+            up_socket.removeAllListeners("close");
+            up_socket.removeAllListeners("error");
             up_socket.pipe(conn, { end: true });
             conn.pipe(up_socket, { end: true });
-        });
-        const destroy = () => {
+        }
+        else
+            up_socket = connectFn({ port: upstream_port, host: upstream_host }, function ()
+            {
+                up_socket.pipe(conn, { end: true });
+                conn.pipe(up_socket, { end: true });
+            });
+        const destroy = () =>
+        {
             up_socket.destroySoon();
             conn.destroySoon();
         };
@@ -29,10 +79,12 @@ function createPipeServer(upstream_port, upstream_host, pipe_type = PipeType.tcp
         up_socket.on('close', destroy);
     });
     if (lisent_port) {
-        server.listen(lisent_port, () => {
+        server.listen(lisent_port, () =>
+        {
             console.log(`启动成功:listen ${pipe_type === PipeType.mtcp2tcp ? "mtcp" : "tcp"}:${lisent_port} -> ${pipe_type === PipeType.mtcp2tcp ? "tcp" : "mtcp"}:${upstream_host}:${upstream_port}`);
         });
-        server.on("error", (err) => {
+        server.on("error", (err) =>
+        {
             console.log(`启动失败:listen:${lisent_port} -> ${upstream_host}:${upstream_port} err:${err.message}`);
         });
     }
