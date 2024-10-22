d3d445c3855ebf92d7cd987498a6d62dfec399a5
ChenX <maple6@vip.qq.com>
2024-10-22 16:58:01 +0800

    注释

--- remote.js
+++ remote.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pipeServer_1 = require("./pipeServer");
 let upstream_host = "0.0.0.0";
 let upstream_port = 5201; //本机反向代理端口
-let lisent_port = 15201;
+let lisent_port = 15201;  //监听端口
 (0, pipeServer_1.createPipeServer)(upstream_port, upstream_host, pipeServer_1.PipeType.mtcp2tcp, lisent_port);
