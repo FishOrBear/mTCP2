69479dde0d8ae5bb45af3abae8ada76ca9052c09
Mike Erickson <cay776@gmail.com>
2025-06-05 15:57:40 +0800

    配置:默认连接数改成3,提升效果

--- client.js
+++ client.js
@@ -6,6 +6,6 @@ let upstream_host = "8.8.8.8"; //远端ip()
 let upstream_port = 15201; //远端mtcp端口
 let lisent_port = 5201; //本地监听端口
 //配置tcp连接个数(每个tcp连接单独一个池子)
-mtcp_1.MSocket.PoolCount = 2; //默认为2 (根据带宽选择 不建议超过5)
+mtcp_1.MSocket.PoolCount = 3; //默认为3 (根据带宽选择 不建议超过5)
 let preLinkCount = 10; //预连接池
 (0, pipeServer_1.createPipeServer)(upstream_port, upstream_host, pipeServer_1.PipeType.tcp2mtcp, lisent_port, mtcp_1.MSocket.PoolCount, preLinkCount);
